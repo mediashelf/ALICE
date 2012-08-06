b7f2b9cd9daf089b3430673d98a7bd61bd9b4503
Devin Walters <devinw@gmail.com>
2012-08-06 14:22:45 -0500

    Remove a bunch of defaults from schema and solrconfig

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -69,32 +69,32 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'title_display', :label => 'Title:'
-    config.add_index_field 'title_vern_display', :label => 'Title:'
-    config.add_index_field 'author_display', :label => 'Author:'
-    config.add_index_field 'author_vern_display', :label => 'Author:'
-    config.add_index_field 'format', :label => 'Format:'
-    config.add_index_field 'language_facet', :label => 'Language:'
-    config.add_index_field 'published_display', :label => 'Published:'
-    config.add_index_field 'published_vern_display', :label => 'Published:'
-    config.add_index_field 'lc_callnum_display', :label => 'Call number:'
+    #config.add_index_field 'title_display', :label => 'Title:'
+    #config.add_index_field 'title_vern_display', :label => 'Title:'
+    #config.add_index_field 'author_display', :label => 'Author:'
+    #config.add_index_field 'author_vern_display', :label => 'Author:'
+    #config.add_index_field 'format', :label => 'Format:'
+    #config.add_index_field 'language_facet', :label => 'Language:'
+    #config.add_index_field 'published_display', :label => 'Published:'
+    #config.add_index_field 'published_vern_display', :label => 'Published:'
+    #config.add_index_field 'lc_callnum_display', :label => 'Call number:'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_display', :label => 'Title:'
-    config.add_show_field 'title_vern_display', :label => 'Title:'
-    config.add_show_field 'subtitle_display', :label => 'Subtitle:'
-    config.add_show_field 'subtitle_vern_display', :label => 'Subtitle:'
-    config.add_show_field 'author_display', :label => 'Author:'
-    config.add_show_field 'author_vern_display', :label => 'Author:'
-    config.add_show_field 'format', :label => 'Format:'
-    config.add_show_field 'url_fulltext_display', :label => 'URL:'
-    config.add_show_field 'url_suppl_display', :label => 'More Information:'
-    config.add_show_field 'language_facet', :label => 'Language:'
-    config.add_show_field 'published_display', :label => 'Published:'
-    config.add_show_field 'published_vern_display', :label => 'Published:'
-    config.add_show_field 'lc_callnum_display', :label => 'Call number:'
-    config.add_show_field 'isbn_t', :label => 'ISBN:'
+    #config.add_show_field 'title_display', :label => 'Title:'
+    #config.add_show_field 'title_vern_display', :label => 'Title:'
+    #config.add_show_field 'subtitle_display', :label => 'Subtitle:'
+    #config.add_show_field 'subtitle_vern_display', :label => 'Subtitle:'
+    #config.add_show_field 'author_display', :label => 'Author:'
+    #config.add_show_field 'author_vern_display', :label => 'Author:'
+    #config.add_show_field 'format', :label => 'Format:'
+    #config.add_show_field 'url_fulltext_display', :label => 'URL:'
+    #config.add_show_field 'url_suppl_display', :label => 'More Information:'
+    #config.add_show_field 'language_facet', :label => 'Language:'
+    #config.add_show_field 'published_display', :label => 'Published:'
+    #config.add_show_field 'published_vern_display', :label => 'Published:'
+    #config.add_show_field 'lc_callnum_display', :label => 'Call number:'
+    #config.add_show_field 'isbn_t', :label => 'ISBN:'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -121,39 +121,39 @@ class CatalogController < ApplicationController
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
 
-    config.add_search_field('title') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+#    config.add_search_field('title') do |field|
+#      # solr_parameters hash are sent to Solr as ordinary url query params.
+#      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+#
+#      # :solr_local_parameters will be sent using Solr LocalParams
+#      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+#      # Solr parameter de-referencing like $title_qf.
+#      # See: http://wiki.apache.org/solr/LocalParams
+#      field.solr_local_parameters = {
+#        :qf => '$title_qf',
+#        :pf => '$title_pf'
+#      }
+#    end
 
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
-      field.solr_local_parameters = {
-        :qf => '$title_qf',
-        :pf => '$title_pf'
-      }
-    end
-
-    config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
-      field.solr_local_parameters = {
-        :qf => '$author_qf',
-        :pf => '$author_pf'
-      }
-    end
+    #config.add_search_field('author') do |field|
+      #field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+      #field.solr_local_parameters = {
+        #:qf => '$author_qf',
+        #:pf => '$author_pf'
+      #}
+    #end
 
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as
     # config[:default_solr_parameters][:qt], so isn't actually neccesary.
-    config.add_search_field('subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
-      field.qt = 'search'
-      field.solr_local_parameters = {
-        :qf => '$subject_qf',
-        :pf => '$subject_pf'
-      }
-    end
+    #config.add_search_field('subject') do |field|
+      #field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+      #field.qt = 'search'
+      #field.solr_local_parameters = {
+        #:qf => '$subject_qf',
+        #:pf => '$subject_pf'
+      #}
+    #end
 
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
