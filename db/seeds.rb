48f65685faba614c7ff93d6dc6aab74546391618
Devin Walters <devinw@gmail.com>
2012-08-27 18:58:33 -0500

    Update seeds to build a basic hierarchy

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,15 @@ def clear_assets_and_index!
   rsolr.commit
 end
 
+def clear_hierarchy!
+  PolicyArea.delete_all
+  SubArea.delete_all
+  Topic.delete_all
+end
+
 def load_assets_from_csv
-  CSV.foreach("#{Rails.root}/support/website_assets_civil_rights_clean.csv", {:headers => true, :header_converters => :symbol}) do |row|
+  opts = {:headers => true, :header_converters => :symbol}
+  CSV.foreach("#{Rails.root}/support/website_assets_civil_rights_clean.csv", opts) do |row|
     Asset.create!(format: row[:format],
                   level: row[:level],
                   policy_area: row[:policy_area],
@@ -34,6 +41,30 @@ def load_assets_from_csv
   end
 end
 
+def build_mock_hierarchy_from_csv
+  opts = {:headers => true, :header_converters => :symbol}
+  CSV.foreach("#{Rails.root}/support/website_assets_civil_rights_clean.csv", opts) do |row|
+    policy_area = PolicyArea.find_by_name(row[:policy_area])
+    sub_area = SubArea.find_by_name(row[:sub_area])
+    topic = Topic.find_by_name(row[:topic])
+    if policy_area
+      if sub_area
+        unless topic
+          sub_area.topics.create!(name: row[:topic])
+        end
+      else
+        policy_area.sub_areas.create!(name: row[:sub_area])
+      end
+    else
+      PolicyArea.create!(name: row[:policy_area])
+    end
+  end
+end
+
 create_admin
+
 clear_assets_and_index!
+clear_hierarchy!
+
+build_mock_hierarchy_from_csv
 load_assets_from_csv
