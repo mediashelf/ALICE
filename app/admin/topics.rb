027203ab8769c736f57c31dab452435feb197e9f
Stephen Anderson and Matt Gauger <dev+bendycode+mathias@bendyworks.com>
2012-08-29 14:11:38 -0500

    Choose sub area associated with topic

--- app/admin/topics.rb
+++ app/admin/topics.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Topic do
-  
+
+  form do |f|
+    f.inputs do
+      f.input :sub_areas, as: :select, collection: options_from_collection_for_select(SubArea.all, 'id', 'select_formatted_name')
+      f.input :name
+      f.input :description
+    end
+    f.buttons
+  end
 end
