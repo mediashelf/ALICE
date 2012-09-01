26987e1811dd9e0c4332677e8b9b83dec61761c1
Ryland Herrick <ryland@bendyworks.com>
2012-08-31 22:03:46 -0500

    Remove old hierarchy bar code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
   include Blacklight::Controller
-  before_filter :show_hierarchy
 
   protect_from_forgery
 
   def layout_name
     'application'
-  end
-
-  def show_hierarchy
-    @show_hierarchy = true
-    @hierarchy_policy_areas = PolicyArea.all
   end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,7 +230,3 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 end
-
-# class Devise::SessionsController < DeviseController
-#   skip_before_filter :show_hierarchy
-# end
